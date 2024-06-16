ed2de59843a463dbf6f494134b1b0e69e5365713
“[firstname <[gmugsk@gmail.com]>
2024-06-16 12:51:27 +0500

    20. Assignment

---
+++ main.js
@@ -0,0 +1,3 @@
+let countries = ["Saudia", "Canada", "USA", "UK", "Palestine"];
+console.log("Countries I would like to visit:", countries);
+export {};
---
+++ main.ts
@@ -0,0 +1,2 @@
+let countries: string[] = ["Saudia", "Canada", "USA", "UK", "Palestine"];
+console.log("Countries I would like to visit:", countries);
